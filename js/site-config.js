c53caf88b67fa9eba95ae02217598e5bf6b5745c
Jörg Kosinski <joerg.kosinski@posteo.de>
2026-08-20 00:01:49 +0200

    Enable Support form with Web3Forms access key.

--- js/site-config.js
+++ js/site-config.js
@@ -2,5 +2,5 @@
  * Create at https://web3forms.com (inbox = your private forward target).
  * No Cloudflare account required. */
 window.GEOWHO_CONTACT = {
-  web3formsAccessKey: "",
+  web3formsAccessKey: "fa2b5121-faf8-457a-b32f-e4985eeede3d",
 };
